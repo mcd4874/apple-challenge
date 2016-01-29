b3b9103a4b714240f14ec654cc05d50a47470d0b
minh duong <mcd4874@g.rit.edu>
2016-01-28 23:33:15 -0500

    Initial Commit

---
+++ Apple ChallengeTests/Apple_ChallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Apple_ChallengeTests.swift
+//  Apple ChallengeTests
+//
+//  Created by minh duong on 1/28/16.
+//  Copyright © 2016 minh duong. All rights reserved.
+//
+
+import XCTest
+@testable import Apple_Challenge
+
+class Apple_ChallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
